1fd3409e08351cf13ac93acfbe08df2a69fd7218
Alex Tu <6798052+AlexTwo@users.noreply.github.com>
2021-08-21 14:49:16 -0400

    breaking: adding nav

--- pypelyne/pages/profile.jsx
+++ pypelyne/pages/profile.jsx
@@ -1,6 +1,7 @@
 import 'tailwindcss/tailwind.css'
 import Link from "next/link";
 import Head from 'next/head'
+import { Navigation } from '../components/Navigation/Navigation';
 
 export default function Profile() {
   return (
@@ -10,9 +11,10 @@ export default function Profile() {
       </Head>
 
       <main className="min-h-screen bg-gradient-to-tr from-green-400 to-blue-500 custom-blur flex items-center justify-center">
-        <section className="glass rounded-3xl">
+        <section id="main" className="glass rounded-3xl">
           
         </section>
+        <Navigation/>
       </main>
     </>
   )
