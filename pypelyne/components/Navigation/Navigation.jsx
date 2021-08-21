a0c6e968ee6f585b27c95b131885f462d011ae49
Alex Tu <6798052+AlexTwo@users.noreply.github.com>
2021-08-21 17:31:11 -0400

    feat: Created Navigation bar

--- pypelyne/components/Navigation/Navigation.jsx
+++ pypelyne/components/Navigation/Navigation.jsx
@@ -1,43 +1,91 @@
 import Link from "next/link";
+import { NavigationItem } from "./NavigationItem";
 
 export const Navigation = () => {
   return (
     <>
       <nav className="flex items-center justify-center flex-wrap p-3 glass rounded-3xl">
         <ul className="flex items-center">
-          <li>
-            <Link href="/">
-              <a className="inline-flex items-center p-2 mr-4">
-                <span>Connect</span>
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <a className="inline-flex items-center p-2 mr-4">
-                <span>Messages</span>
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <a className="inline-flex items-center p-2 mr-4">
-                <span>Likes</span>
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <a className="inline-flex items-center p-2 mr-4">
-                <span>Profile</span>
-              </a>
-            </Link>
-          </li>
+          <NavigationItem
+            link="/"
+            title="Connect"
+            icon={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
+                />
+              </svg>
+            }
+          />
+          <NavigationItem
+            link="/"
+            title="Messages"
+            icon={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                />
+              </svg>
+            }
+          />
+          <NavigationItem
+            link="/"
+            title="Likes"
+            icon={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            }
+          />
+          <NavigationItem
+            link="/"
+            title="Profile"
+            icon={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            }
+          />
         </ul>
       </nav>
       <style jsx>{`
         nav {
-          // position: fixed;
           min-height: 5vh;
           width: 60%;
           padding: 1rem 3rem;
