a6649c5e4553ec3944b87e428e8d09e75bdf6daa
Alex Tu <6798052+AlexTwo@users.noreply.github.com>
2021-08-21 03:16:20 -0400

    setting up components, app & profile

---
+++ pypelyne/components/layouts/layout.js
@@ -0,0 +1,5 @@
+// components/layouts/layout.js 
+
+// NavBar
+
+// Background
--- pypelyne/pages/_app.js
+++ pypelyne/pages/_app.js
@@ -1,7 +1,12 @@
 import 'tailwindcss/tailwind.css'
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>      
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp
