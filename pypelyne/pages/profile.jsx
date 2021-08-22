98ce392c0c8c5894348ad522a169765cf2e5ad86
Alex Tu <6798052+AlexTwo@users.noreply.github.com>
2021-08-22 03:33:14 -0400

    breaking: created a dropdown menu

--- pypelyne/pages/profile.jsx
+++ pypelyne/pages/profile.jsx
@@ -2,10 +2,11 @@ import "tailwindcss/tailwind.css";
 import Link from "next/link";
 import Head from "next/head";
 import { Navigation } from "../components/Navigation/Navigation";
-import {useAuthState} from "react-firebase-hooks/auth";
-import {auth} from "../components/firebase/firebase";
-import {useEffect} from "react";
-import {useRouter} from "next/router";
+import Dropdowns from "../components/Dropdowns";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../components/firebase/firebase";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 import { ProfilePicture } from "../components/Profile/ProfilePicture";
 import { Tag } from "../components/Tags/Tag";
@@ -18,7 +19,7 @@ export default function Profile() {
     if (loading) {
       return;
     }
-    if (!user) router.replace('/');
+    if (!user) router.replace("/");
   }, [user, loading]);
 
   return (
@@ -39,7 +40,7 @@ export default function Profile() {
             textColour="text-gray-600"
           />
         </section>
-
+        <Dropdowns />
         <Navigation />
       </main>
     </>
