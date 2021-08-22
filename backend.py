75ba304a8eebddcaa3d7490af3ff83c5dc1aaf08
HDCat2 <henningjiang888+2@gmail.com>
2021-08-22 05:37:22 -0400

    Update backend.py
    fill out compare and main

--- backend.py
+++ backend.py
@@ -13,25 +13,47 @@ def compare(conn,uid1,uid2):
     if not bdf.empty:
         score = -1
         return score
-
-    if not ldf.empty:
-        score += 1000000
     
     taglist1 = [udf.at[0,'tag%d_id' % i] for i in range(1,9) if udf.at[0,'tag%d_id' % i] is not None]
     taglist2 = [udf.at[1,'tag%d_id' % i] for i in range(1,9) if udf.at[1,'tag%d_id' % i] is not None]
-    print(taglist1)
-    print(taglist2)
+    
+    for i in taglist1:
+        if i in taglist2:
+            score += 100
+
+    loc1 = udf.at[0, 'location_id']
+    loc2 = udf.at[1, 'location_id']
+
+    if loc1 is not None and loc2 is not None:
+        score += 300
+    
+    if score == 0:
+        return score
+
+    if not ldf.empty:
+        score += 1000000
 
     return score
 
 
 def main():
     conn = get_connection()
+    #uid = sys.argv[0]
+    uid = 686639686897805073
 
-    compare(conn, 686639686897805073, 686643247083267857)
+    df = pd.read_sql(con = conn, sql="SELECT * FROM PROFILE")
+    matches = []
+    for i in range(len(df.index)):
+        if df.at[i, 'id'] != uid:
+            res = compare(conn, uid, df.at[i, 'id'])
+            if res > 0:
+                matches.append((res, uid))
+    
+    matches.sort(reverse = True)
+    #compare(conn, 686639686897805073, 686643247083267857)
 
     conn.close()
-
+    return matches
 
 if __name__ == "__main__":
     main()
