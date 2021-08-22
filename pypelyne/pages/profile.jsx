9581b0bbdf15164d770f8085bbb98596bb5be1a8
Alex Tu <6798052+AlexTwo@users.noreply.github.com>
2021-08-21 21:27:11 -0400

    fix: set placeholder image for profile picture

--- pypelyne/pages/profile.jsx
+++ pypelyne/pages/profile.jsx
@@ -2,8 +2,8 @@ import "tailwindcss/tailwind.css";
 import Link from "next/link";
 import Head from "next/head";
 import { Navigation } from "../components/Navigation/Navigation";
-import { ProfilePicture } from "../components/Profile/Profile";
-
+import { ProfilePicture } from "../components/Profile/ProfilePicture";
+import { Tag } from "../components/Tags/Tag";
 export default function Profile() {
   return (
     <>
@@ -13,16 +13,17 @@ export default function Profile() {
 
       <main className="min-h-screen bg-gradient-to-tr from-green-400 to-blue-500 custom-blur flex items-center justify-center flex-col">
         <section id="main" className="glass rounded-3xl m-2">
-          {/* <div className="glass rounded-2xl w-52 h-52 m-auto mt-4"></div> */}
           <ProfilePicture
-            image="https://alextu2.s-ul.eu/v752lajE"
-            className="glass rounded-2xl w-52 h-52 m-auto mt-4 z-50 relative"
+            image="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png"
+            className="glass rounded-2xl w-52 h-52 m-auto mt-4"
           />
-          <div
-            className="glass rounded-2xl w-4/5 h-28 m-auto mt-4 z-40 relative"
-            style={{ transform: "translateY(-8rem)" }}
-          ></div>
+          <Tag
+            tagText="C++"
+            tagColour="bg-red-300"
+            textColour="text-gray-600"
+          />
         </section>
+
         <Navigation />
       </main>
     </>
