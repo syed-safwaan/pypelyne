ed5750da50b1a114c860c47f9af4eda3148d95ff
Alex Tu <6798052+AlexTwo@users.noreply.github.com>
2021-08-22 00:19:05 -0400

    feat: added tinder like swiping cards

--- pypelyne/pages/connect.jsx
+++ pypelyne/pages/connect.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useMemo} from "react";
+import React, { useState, useMemo } from "react";
 import TinderCard from "react-tinder-card";
 import styles from "../assets/connect.module.scss";
 
@@ -67,196 +67,207 @@ function Advanced() {
   };
 
   return (
-  <div>
-    <style jsx>{`
-    #root {
-      text-align: center;
-      display: flex;
-      justify-content: center;
-      width: 100vw;
-      min-height: 100vh;
-      overflow: hidden;
-      /* background: linear-gradient(#fff, #999); */
-      background: linear-gradient(#e66465, #9198e5);
-    }
+    <div>
+      <style jsx>{`
+        #root {
+          text-align: center;
+          display: flex;
+          justify-content: center;
+          width: 100vw;
+          min-height: 100vh;
+          overflow: hidden;
+          /* background: linear-gradient(#fff, #999); */
+          background: linear-gradient(#e66465, #9198e5);
+        }
 
-    #root > div {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-    }
-  
-    .app > div {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-    }
-  
-    .row {
-      flex-direction: row !important;
-    }
-  
-    .row > * {
-      margin: 5px;
-    }
-  
-    h1 {
-      font-family: "Damion", cursive;
-      color: #fff;
-      text-shadow: 0px 0px 60px 0px rgba(0, 0, 0, 0.3);
-    }
-  
-    h2 {
-      color: #fff;
-    }
-  
-    .swipe {
-      position: absolute;
-    }
-  
-    .cardContainer {
-      width: 90vw;
-      max-width: 260px;
-      height: 300px;
-    }
-  
-    .card {
-      position: absolute;
-      background-color: #fff;
-      width: 80vw;
-      max-width: 260px;
-      height: 300px;
-      box-shadow: 0px 0px 60px 0px rgba(0, 0, 0, 0.3);
-      border-radius: 20px;
-      background-size: cover;
-      background-position: center;
-    }
-  
-    .cardContent {
-      width: 100%;
-      height: 100%;
-    }
-  
-    .swipe:last-of-type {
-    }
-  
-    .card h3 {
-      position: absolute;
-      bottom: 0;
-      margin: 10px;
-      color: #fff;
-    }
-  
-    .infoText {
-      width: 100%;
-      height: 28px;
-      justify-content: center;
-      display: flex;
-      color: #fff;
-      animation-name: popup;
-      animation-duration: 800ms;
-    }
-  
-    .buttons {
-      margin: 20px;
-      display: flex;
-    }
-  
-    .buttons button {
-      flex-shrink: 0;
-      padding: 10px;
-      border-radius: 5px;
-      border: none;
-      color: #fff;
-      font-size: 18px;
-      background-color: #9198e5;
-      transition: 200ms;
-      margin: 0 10px;
-      font-weight: bolder;
-      width: 160px;
-      box-shadow: 0px 0px 30px 0px rgba(0, 0, 0, 0.1);
-    }
-  
-    .buttons button:hover {
-      transform: scale(1.05);
-    }
-  
-    @keyframes popup {
-      0% {
-        transform: scale(1, 1);
-      }
-      10% {
-        transform: scale(1.1, 1.1);
-      }
-      30% {
-        transform: scale(0.9, 0.9);
-      }
-      50% {
-        transform: scale(1, 1);
-      }
-      57% {
-        transform: scale(1, 1);
-      }
-      64% {
-        transform: scale(1, 1);
-      }
-      100% {
-        transform: scale(1, 1);
-      }
-    }
-    
-    #connect {
-      margin: 0;
-    }
-    `}</style>
+        #root > div {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        body {
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+            "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+            "Helvetica Neue", sans-serif;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+        }
 
-    <div id="connect">
-    <link
-      href="https://fonts.googleapis.com/css?family=Damion&display=swap"
-      rel="stylesheet"
-    />
-    <link
-      href="https://fonts.googleapis.com/css?family=Alatsi&display=swap"
-      rel="stylesheet"
-    />
-    <h1>React Tinder Card</h1>
-    <div className="cardContainer">
-      {characters.map((character, index) => (
-        <TinderCard
-          ref={childRefs[index]}
-          className="swipe"
-          key={character.name}
-          onSwipe={(dir) => swiped(dir, character.name)}
-          onCardLeftScreen={() => outOfFrame(character.name)}
-        >
-          <div
-            style={{backgroundImage: "url(" + character.url + ")"}}
-            className="card"
-          >
-            <h3>{character.name}</h3>
+        code {
+          font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+            monospace;
+        }
+
+        .app > div {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        .row {
+          flex-direction: row !important;
+        }
+
+        .row > * {
+          margin: 5px;
+        }
+
+        h1 {
+          font-family: "Damion", cursive;
+          color: #fff;
+          text-shadow: 0px 0px 60px 0px rgba(0, 0, 0, 0.3);
+        }
+
+        h2 {
+          color: #fff;
+        }
+
+        .swipe {
+          position: absolute;
+        }
+
+        .cardContainer {
+          width: 90vw;
+          max-width: 260px;
+          height: 300px;
+        }
+
+        .card {
+          position: absolute;
+          background-color: #fff;
+          width: 80vw;
+          max-width: 260px;
+          height: 300px;
+          box-shadow: 0px 0px 60px 0px rgba(0, 0, 0, 0.3);
+          border-radius: 20px;
+          background-size: cover;
+          background-position: center;
+        }
+
+        .cardContent {
+          width: 100%;
+          height: 100%;
+        }
+
+        .swipe:last-of-type {
+        }
+
+        .card h3 {
+          position: absolute;
+          bottom: 0;
+          margin: 10px;
+          color: #fff;
+        }
+
+        .infoText {
+          width: 100%;
+          height: 28px;
+          justify-content: center;
+          display: flex;
+          color: #fff;
+          animation-name: popup;
+          animation-duration: 800ms;
+        }
+
+        .buttons {
+          margin: 20px;
+          display: flex;
+        }
+
+        .buttons button {
+          flex-shrink: 0;
+          padding: 10px;
+          border-radius: 5px;
+          border: none;
+          color: #fff;
+          font-size: 18px;
+          background-color: #9198e5;
+          transition: 200ms;
+          margin: 0 10px;
+          font-weight: bolder;
+          width: 160px;
+          box-shadow: 0px 0px 30px 0px rgba(0, 0, 0, 0.1);
+        }
+
+        .buttons button:hover {
+          transform: scale(1.05);
+        }
+
+        @keyframes popup {
+          0% {
+            transform: scale(1, 1);
+          }
+          10% {
+            transform: scale(1.1, 1.1);
+          }
+          30% {
+            transform: scale(0.9, 0.9);
+          }
+          50% {
+            transform: scale(1, 1);
+          }
+          57% {
+            transform: scale(1, 1);
+          }
+          64% {
+            transform: scale(1, 1);
+          }
+          100% {
+            transform: scale(1, 1);
+          }
+        }
+      `}</style>
+      <div id="root">
+        <div className="app">
+          <div>
+            <link
+              href="https://fonts.googleapis.com/css?family=Damion&display=swap"
+              rel="stylesheet"
+            />
+            <link
+              href="https://fonts.googleapis.com/css?family=Alatsi&display=swap"
+              rel="stylesheet"
+            />
+            <h1>React Tinder Card</h1>
+            <div className="cardContainer">
+              {characters.map((character, index) => (
+                <TinderCard
+                  ref={childRefs[index]}
+                  className="swipe"
+                  key={character.name}
+                  onSwipe={(dir) => swiped(dir, character.name)}
+                  onCardLeftScreen={() => outOfFrame(character.name)}
+                >
+                  <div
+                    style={{ backgroundImage: "url(" + character.url + ")" }}
+                    className="card"
+                  >
+                    <h3>{character.name}</h3>
+                  </div>
+                </TinderCard>
+              ))}
+            </div>
+            <div className="buttons">
+              <button onClick={() => swipe("left")}>Swipe left!</button>
+              <button onClick={() => swipe("right")}>Swipe right!</button>
+            </div>
+            {lastDirection ? (
+              <h2 key={lastDirection} className="infoText">
+                You swiped {lastDirection}
+              </h2>
+            ) : (
+              <h2 className="infoText">
+                Swipe a card or press a button to get started!
+              </h2>
+            )}
           </div>
-        </TinderCard>
-      ))}
+        </div>
+      </div>
     </div>
-    <div className="buttons">
-      <button onClick={() => swipe("left")}>Swipe left!</button>
-      <button onClick={() => swipe("right")}>Swipe right!</button>
-    </div>
-    {lastDirection ? (
-      <h2 key={lastDirection} className="infoText">
-        You swiped {lastDirection}
-      </h2>
-    ) : (
-      <h2 className="infoText">
-        Swipe a card or press a button to get started!
-      </h2>
-    )}
-  </div>
-  </div>
-)
-  ;
+  );
 }
 
 export default Advanced;
