f02015a9838aaea937ee019dc92151dc8bac4bb2
Syed Safwaan <syed-safwaan@wayne.edu>
2021-08-22 03:12:30 -0400

    Cursor db operations

--- backend.py
+++ backend.py
@@ -3,9 +3,20 @@ import psycopg2
 import pandas as pd
 from getConnection import get_connection
 
+us_df = pd.read_csv("../US/uscities.csv")
+ca_df = pd.read_csv("../CA/canadacities.csv")
 
 def main():
     conn = get_connection()
+    cur = conn.cursor()
+
+    for row in us_df.itertuples():
+        sql = "INSERT INTO LOCATION (CITY, REGION, COUNTRY) VALUES (%s, %s, %s) RETURNING ID;"
+        cur.execute(sql, (row.city_ascii, row.state_id, "US"))
+        # print(cur.fetchone()[0])
+    conn.commit()
+    cur.close()
+
 
     df = pd.read_sql(con=conn, sql="SHOW TABLES")
     print(df)
