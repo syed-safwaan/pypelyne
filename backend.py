d381391fc9528c9b3e4b6dc052b03f446ee9453d
HDCat2 <henningjiang888+2@gmail.com>
2021-08-22 04:43:20 -0400

    Update backend.py
    adding beginnings of comparisons

--- backend.py
+++ backend.py
@@ -6,9 +6,21 @@ from getConnection import get_connection
 
 def compare(conn,uid1,uid2):
     score = 0
-    udf = pd.read_sql(con = conn, sql="SELECT * FROM profile WHERE id=%d OR id = %d" % (uid1,uid2))
+    udf = pd.read_sql(con = conn, sql="SELECT * FROM PROFILE WHERE id=%d OR id = %d" % (uid1,uid2))
+    ldf = pd.read_sql(con = conn, sql="SELECT * FROM LIKING WHERE LIKER_ID=%d AND LIKED_ID=%d" % (uid1,uid2))
+    bdf = pd.read_sql(con = conn, sql="SELECT * FROM BLOCKING WHERE (BLOCKER_ID=%d AND BLOCKED_ID=%d) OR (BLOCKER_ID=%d AND BLOCKED_ID=%d)" % (uid1,uid2,uid2,uid1))
 
-    print(udf)
+    if not bdf.empty:
+        score = -1
+        return score
+
+    if not ldf.empty:
+        score += 1000000
+    
+    taglist1 = [udf.at[0,'tag%d_id' % i] for i in range(1,9) if udf.at[0,'tag%d_id' % i] is not None]
+    taglist2 = [udf.at[1,'tag%d_id' % i] for i in range(1,9) if udf.at[1,'tag%d_id' % i] is not None]
+    print(taglist1)
+    print(taglist2)
 
     return score
 
